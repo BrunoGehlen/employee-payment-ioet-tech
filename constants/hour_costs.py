5fba7d6d6f2879965a326580895b7f1eb011ea10
BrunoGehlen <tousepc@gmail.com>
2023-02-21 02:03:46 -0300

    feat: add static files

---
+++ constants/hour_costs.py
@@ -0,0 +1,16 @@
+"""
+Defines hourly rates for different shifts during work days and weekends.
+
+Regular hours: 09:01 - 18:00
+Evening hours: 18:01 - 00:00
+Dawn hours: 00:01 - 09:00
+
+"""
+
+WORK_DAY_REGULAR_HOURS_RATE = 15
+WORK_DAY_EVENING_HOURS_RATE = 20
+WORK_DAY_DAWN_HOURS_RATE = 25
+
+WEEKEND_REGULAR_HOURS_RATE = 20
+WEEKEND_EVENING_HOURS_RATE = 25
+WEEKEND_DAWN_HOURS_RATE = 30
